f4b043a02bcb1996033e1454223cdd7e5bcf8b5b
Victor Nicolini de Queiroz Santos <victor.nicolinivnqs@gmail.com>
2025-01-13 19:24:36 -0300

    Commit inicial do projeto

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Função para compilar arquivos SCSS para CSS
+function compilaSass() {
+    return gulp.src('./source/styles/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+// Função para otimizar imagens
+function otimizaImagens() {
+    return gulp.src('source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images'));
+}
+
+// Função para comprimir imagens
+function comprimeImagens() {
+    return gulp.src('source/images/*')
+        .pipe(imagemin()) // Otimiza as imagens sem especificações
+        .pipe(gulp.dest('./build/images')); // Destino das imagens otimizadas
+}
+
+// Exporta as tarefas para que possam ser chamadas pelo Gulp
+exports.sass = compilaSass;
+exports.imagemin = otimizaImagens;
+exports.comprimeImagens = comprimeImagens;
+
+// Tarefa padrão que observa mudanças nos arquivos SCSS e imagens
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(otimizaImagens));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
+    console.log('Gulp está observando seus arquivos!');
+};
