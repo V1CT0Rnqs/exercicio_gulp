860e5da17bd69d004b6abcb3785fd66c4b2074bf
Victor Nicolini de Queiroz Santos <victor.nicolinivnqs@gmail.com>
2025-01-13 20:27:58 -0300

    versionamento do código

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,47 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
+const newer = require('gulp-newer');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber'); // Adicionado para tratamento de erros
 
 // Função para compilar arquivos SCSS para CSS
 function compilaSass() {
     return gulp.src('./source/styles/*.scss')
+        .pipe(plumber()) // Adicionado para tratamento de erros
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./build/styles'));
+        .pipe(gulp.dest('./build/styles/*.css'));
 }
 
 // Função para otimizar imagens
 function otimizaImagens() {
-    return gulp.src('source/images/*')
+    return gulp.src('./source/images/*')
+        .pipe(plumber()) // Adicionado para tratamento de erros
+        .pipe(newer('./build/images')) // Verifica se há arquivos novos ou modificados
         .pipe(imagemin())
-        .pipe(gulp.dest('build/images'));
+        .pipe(gulp.dest('./build/images'));
+}
+
+// Função para minificar JavaScript
+function minificaJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(plumber()) // Adicionado para tratamento de erros
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'));
 }
 
 // Exporta as tarefas para que possam ser chamadas pelo Gulp
 exports.sass = compilaSass;
 exports.imagemin = otimizaImagens;
-exports.comprimeImagens = comprimeImagens;
+exports.minifyjs = minificaJS;
 
-// Tarefa padrão que observa mudanças nos arquivos SCSS e imagens
+// Tarefa padrão que observa mudanças nos arquivos SCSS, imagens e scripts
 exports.default = function() {
-    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
-    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(otimizaImagens));
-    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
-    console.log('Gulp está observando seus arquivos!');
+    gulp.watch('./source/styles/*.scss', gulp.series(compilaSass));
+    gulp.watch('./source/images/*', gulp.series(otimizaImagens));
+    gulp.watch('./source/scripts/*.js', gulp.series(minificaJS));
 };
---
+++ source/scripts/sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+
+// Exemplo de uso
+console.log(sum(3, 4));r
