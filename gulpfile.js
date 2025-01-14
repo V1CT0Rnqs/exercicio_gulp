5dca9546d02bf9774bb85a83d78f12b62fdc6808
Victor Nicolini de Queiroz Santos <victor.nicolinivnqs@gmail.com>
2025-01-14 20:17:56 -0300

    reparo de código

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,41 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
-const newer = require('gulp-newer');
 const uglify = require('gulp-uglify');
-const plumber = require('gulp-plumber'); // Adicionado para tratamento de erros
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+// Função para minificar JavaScript
+function comprimeJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'));
+}
 
 // Função para compilar arquivos SCSS para CSS
 function compilaSass() {
     return gulp.src('./source/styles/*.scss')
-        .pipe(plumber()) // Adicionado para tratamento de erros
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
-        }).on('error', sass.logError))
+        }))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./build/styles/*.css'));
+        .pipe(gulp.dest('./build/styles'));
 }
 
 // Função para otimizar imagens
-function otimizaImagens() {
+function comprimeImagens() {
     return gulp.src('./source/images/*')
-        .pipe(plumber()) // Adicionado para tratamento de erros
-        .pipe(newer('./build/images')) // Verifica se há arquivos novos ou modificados
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'));
 }
 
-// Função para minificar JavaScript
-function minificaJS() {
-    return gulp.src('./source/scripts/*.js')
-        .pipe(plumber()) // Adicionado para tratamento de erros
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/scripts'));
-}
-
-// Exporta as tarefas para que possam ser chamadas pelo Gulp
-exports.sass = compilaSass;
-exports.imagemin = otimizaImagens;
-exports.minifyjs = minificaJS;
+exports.styles = compilaSass;
 
 // Tarefa padrão que observa mudanças nos arquivos SCSS, imagens e scripts
 exports.default = function() {
     gulp.watch('./source/styles/*.scss', gulp.series(compilaSass));
-    gulp.watch('./source/images/*', gulp.series(otimizaImagens));
-    gulp.watch('./source/scripts/*.js', gulp.series(minificaJS));
+    gulp.watch('./source/scripts/*.js', gulp.series(comprimeJS));
+    gulp.watch('./source/images/*', gulp.series(comprimeImagens));
 };
